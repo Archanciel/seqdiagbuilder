7361ec70ab721ff98fc9e94c23ccb01e718ca101
Archanciel <jp.schnyder@gmail.com>
2019-06-02 19:46:07 +0200

    new test case

--- test/testloopindexdictionary.py
+++ test/testloopindexdictionary.py
@@ -13,6 +13,10 @@ from seqdiagbuilder import LoopIndexDictionary
 
 class TestLoopIndexDictionary(unittest.TestCase):
     def testLoopIndexDictionaryClassLoopNestedInnerOne(self):
+        '''
+        Test the correct working of the LoopIndexDictionary.
+        :return:
+        '''
         loopIdxDic = LoopIndexDictionary()
         sourcePathFileName = parentdir + "\\testclasses\\classloopnestedinnerone.py"
         className = 'ClassLoopNestedInnerOne'
@@ -29,6 +33,34 @@ class TestLoopIndexDictionary(unittest.TestCase):
         value_17 = loopIdxDic.get(key_17)
         self.assertEqual(len(value_17), 2)
         self.assertEqual(value_17[0], [':seqdiag_loop_start', '3 times'], [':seqdiag_loop_start_end', '5 times'])
+
+        key_20 = loopIdxDic.buildKey(className, methodName, 'doC2', 20)
+        self.assertIsNotNone(key_20)
+        value_20 = loopIdxDic.get(key_20)
+        self.assertEqual(value_20[0], [':seqdiag_loop_end', None])
+
+    def testLoopIndexDictionaryClassLoopNestedInnerOneNoTimeInfo(self):
+        '''
+        Test the correct working of the LoopIndexDictionary for loop tags
+        with no time info.
+        :return:
+        '''
+        loopIdxDic = LoopIndexDictionary()
+        sourcePathFileName = parentdir + "\\testclasses\\classloopnestedinneronefortestloopidxdic.py"
+        className = 'ClassLoopNestedInnerOneForTestLoopIdxDic'
+        methodName = 'doB'
+
+        with open(sourcePathFileName, "r") as f:
+            contentList = f.readlines()
+            methodDefLineIndex = [i for (i, entry) in enumerate(contentList) if methodName in entry][0]
+            loopIdxDic.storeLoopCommands(className, methodName, methodDefLineIndex + 1, [contentList[methodDefLineIndex:]])
+
+        key_17 = loopIdxDic.buildKey(className, methodName, 'doCWithNote', 17)
+
+        self.assertIsNotNone(key_17)
+        value_17 = loopIdxDic.get(key_17)
+        self.assertEqual(len(value_17), 2)
+        self.assertEqual(value_17[0], [':seqdiag_loop_start', None], [':seqdiag_loop_start_end', None])
 
         key_20 = loopIdxDic.buildKey(className, methodName, 'doC2', 20)
         self.assertIsNotNone(key_20)
---
+++ testclasses/classloopnestedinneronefortestloopidxdic.py
@@ -0,0 +1,24 @@
+from testclasses.classleaf import ClassLeaf
+
+class ClassLoopNestedInnerOneForTestLoopIdxDic:
+    '''
+    This class is used to test the LoopIndexDictionary. The loop tag have
+    no time info.
+
+    '''
+
+    def doB(self, p1):
+        c = ClassLeaf()
+        a = 0
+
+        for i in range(3):
+            a += 1 # dummy instruction
+            for i in range(5):
+                c.doCWithNote(p1) #:seqdiag_loop_start :seqdiag_loop_start_end
+                a += 1 # dummy instruction
+            a += 1 # dummy instruction
+            c.doC2(p1) #:seqdiag_loop_end
+            a += 1 # dummy instruction
+
+        c.doC1(p1)
+        print(a) # another dummy instruction
